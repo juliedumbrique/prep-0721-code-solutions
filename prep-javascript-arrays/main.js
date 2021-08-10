2f3d482f81cc6f5442478a7042aec7902cd4b71a
Julie Dumbrique <julie.dumbrique@gmail.com>
2021-08-10 16:17:31 -0700

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,22 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+
+console.log(languages);
+
+const firstElement = languages[0];
+console.log(firstElement);
+
+const thirdElement = languages[2];
+console.log(thirdElement);
+
+const length = languages.length;
+console.log(length);
+
+const lastElement = languages[5];
+console.log(lastElement);
