d5e1657f704430610f33739c915ce0b6b6f572ff
Julie Dumbrique <julie.dumbrique@gmail.com>
2021-08-11 11:52:57 -0700

    Completed subtractTwoNumbers function definition.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -1,6 +1,6 @@
-function multiplyAndDivideBy5(num1, num2) {
-  return (num1 * num2) / 5;
+function subtractTwoNumbers(num1, num2) {
+  return (num1 - num2);
 }
 
-var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
-console.log(multiplyAndDivideBy5Result);
+var subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+console.log(subtractTwoNumbersResult);
