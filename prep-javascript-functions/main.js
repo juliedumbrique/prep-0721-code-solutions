8a4b99ae15148dec37a83d22b0cd0fb2cb54bf26
Julie Dumbrique <julie.dumbrique@gmail.com>
2021-08-11 11:28:20 -0700

    Completed multiplyAndDivideBy5 function definition.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -1,6 +1,6 @@
-function addAndMultiplyBy5(num1, num2) {
-  return (num1 + num2) * 5;
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
 }
 
-var addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
-console.log(addAndMultiplyBy5Result);
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+console.log(multiplyAndDivideBy5Result);
